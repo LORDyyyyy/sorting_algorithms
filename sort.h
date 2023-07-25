56920f026a8c75e4808f83fb62a65f00d3207e8f
LORDyyyyy <mlord6271@gmail.com>
2023-07-26 02:03:57 +0300

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -32,4 +32,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
 
+void quick_sort(int *array, size_t size);
+void quick_sort_rec(int *array, int lower, int higher, size_t size);
+int lomuto_partition(int *array, int lower, int higher, size_t size);
+
 #endif
